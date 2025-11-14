b3072378bea6b0167d1ddb1c08a53cd233687016
João Paulo Guedes Carvalho <joaopaullogcarvalho@gmail.com>
2025-11-13 21:49:45 -0300

    refactor(router): implement granular role-based route protection

--- app/src/components/PrivateRouter.tsx
+++ app/src/components/PrivateRouter.tsx
@@ -4,10 +4,32 @@ import type { JSX } from "react";
 
 interface PrivateRouterProps{
     children: JSX.Element;
+    allowedRoles?: string[];
 }
 
-export const PrivateRouter = ({ children }: PrivateRouterProps) => {
-    const { isAuthenticated } = useAuth();
+export const PrivateRouter = ({ children, allowedRoles }: PrivateRouterProps) => {
+    const { isAuthenticated, user } = useAuth();
 
-    return isAuthenticated ? children : <Navigate to={"/"} replace />;
+    if(!isAuthenticated) return <Navigate to="/login" replace />
+
+    if(allowedRoles && user && !allowedRoles.includes(user.role)){
+        switch (user.role){
+            case "admin":
+                return <Navigate to="/admin" replace />
+            case "operador":
+                return <Navigate to="/operador" replace />
+            case "qualidade":
+                return <Navigate to="/quality-dashboard" replace />
+            case "engenharia":
+                return <Navigate to="/engineering-dashboard" replace />
+            case "tecnico_fundicao":
+                return <Navigate to="/tec-fundicao" replace />
+            case "tecnico_usinagem":
+                return <Navigate to="/tec-usinagem" replace />
+            default:
+                return <Navigate to="/login" replace />
+        }
+    }
+
+    return children
 };
